0a94fefb57782a719876483b10ae5292ee4157ce
elokrabek <mikus.taki2005@onet.pl>
2022-01-16 20:31:24 +0100

    lazuryt

--- scripts/main/gra.py
+++ scripts/main/gra.py
@@ -8,10 +8,11 @@ if __name__ == '__main__':
 
 player = FirstPersonController(scale=1.5) #(model="postac", texture="postac.png", scale=1.5, collider="mesh")
 player.cursor = Entity(parent=camera.ui, model ="circle", scale=0.01, color =color.black)
-gun = Entity(model='latarka',texture="latarka.png", parent=camera, position=(0.1, -0.1, 0.2), scale=0.1,)
+gun = LitObject(model='latarka',texture="latarka.png", parent=camera, position=(0.1, -0.1, 0.2), scale=0.1,)
 
 player = FirstPersonController()
 player.cursor = Entity(parent=camera.ui, model="circle", color =color.black, scale=.009)
+
 
 def update():
     if held_keys['shift']:
@@ -32,10 +33,11 @@ def entities():
     drzewo = LitObject(model="drzewo", texture="drzewo.png", scale=2, position=(-3, 1, 2))
     szafka = LitObject(model="szafka", texture="szafka.png", collider="box", scale=3, position=(3, 2, 2.2))
     skybox = Sky(model="sphere", double_sided=True, texture="load", rotation=(0, 90, 0))
-    biurko = LitObject(model="biurko", texture = "biurko.png", collider="box", scale = Vec3(2.3,2,2.4), position=(-3.51, 0.1, -4.7),
+    biurko = LitObject(model="biurko", texture = "biurko.png", collider="box", scale = Vec3(2.3,4,2.4), position=(-3.51, 0.1, -4.7),
                     rotation=Vec3(0, 90, 0))
+    biurko.collider = BoxCollider(biurko, size=Vec3(1.2,3,2))
     sciany = LitObject(model="sciany", texture="sciana.jpg", collider="mesh", scale=3, position=(0, 0, 0))
-    sufit = LitObject(model="sufit", texture="sciana.jpg", collider="mesh", scale=3, position=(0, 0, 0))
+    sufit = LitObject(model="sufit", texture="sciana.jpg", scale=3, position=(0, 0, 0))
     lozko = LitObject(model="lozko", texture="lozko.png", collider="mesh", scale=1.5, position=(-2.7, 0, 1.4),
                    rotation=Vec3(0, 90, 0))
     schody = LitObject(model="schody", texture="schody.png", collider="mesh", scale = Vec3(1,0.7,1), position=(4.5, 0.8, -5.6),
@@ -43,14 +45,14 @@ def entities():
     podloga = LitObject(model="podloga", texture="podloga.png", collider="mesh", scale=3, position=(0, 0.01, 0),
                    rotation=Vec3(0, 0, 0))
     listwa = LitObject(model="listwa", texture="podloga.png", collider="mesh", scale=3, position=(0, 0, 0))
-    krzeslo = LitObject(model="krzeslo", texture="krzeslo.png", collider="box", scale=2.2, position=(-3.51, 0.2, -3.7), rotation=Vec3(0, 180, 0))
-    latarka = LitObject(model="latarka", texture="latarka.png", collider="mesh", scale=0.3, position=(3.76, 2.1, 2.3), rotation=Vec3(0, 90, 0))
-    krzeslo = LitObject(model="krzeslo", texture="krzeslo.png", collider="box", scale=2.2, position=(-3.51, 0.2, -3.7), rotation=Vec3(0, 180, 0))
+    latarka = LitObject(model="latarka", texture="latarka.png", collider="box", scale=0.3, position=(3.76, 2.1, 2.3), rotation=Vec3(0, 90, 0))
+    krzeslo = LitObject(model="krzeslo", texture="krzeslo.png", collider="mesh", scale=2.2, position=(-3.5, 0.2, -3.3), rotation=Vec3(0, 180, 0))
     okno = LitObject(model="okno", texture="okno.png", collider="mesh", scale=3, position=(-6.3, 4.6, -3.75))
     drzwi = LitObject(model="drzwi", texture="drzwi.png", collider="mesh", scale = Vec3(3,2.95,3), position=(0, 0.12, 0))
-    listwa = LitObject(model="listwa2", texture="listwa2.png", collider="mesh", scale=3, position=(0, 0, 0))
-#toDo okno naprawic bo jest zjebane
-
+    listwa = LitObject(model="listwa2", texture="listwa2.png", scale=3, position=(0, 0, 0))
+    klawa = LitObject(model="klawa", texture="klawa.png", scale=0.5, position=(-3.4, 1.83, -4.2))
+    monitor = LitObject(model="monitor", texture="monitor.png", scale=1.1, position=(-3.4, 2.6, -5), rotation=Vec3(0, 90, 0))
+    komputer = LitObject(model="komputer", texture="komputer.png", scale=0.8, position=(-5.3, 2.4, -4.8), rotation=Vec3(0, 90, 0))
 def run():
 
 
