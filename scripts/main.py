ec35e8923d40b90428327d0b04b8091837390bcc
Iieitaimus <dawid092004@onet.pl>
2022-01-18 00:07:36 +0100

    psycha nisko

---
+++ scripts/main.py
@@ -0,0 +1,26 @@
+import time
+from ursina import *
+from gra import entities
+from mainn_menu import MainMenu
+from loading_screan import LoadingScreen
+
+
+class Game(Ursina):
+    def __init__(self, spawn):
+        super().__init__()
+        self.spawn = spawn
+        self.mm = MainMenu(play=self.play)      #Sequence(Wait(.01), Func(sys.exit))
+        self.load = None
+
+    def play(self):
+        self.mm.cleanDel()
+        self.load = LoadingScreen()
+
+        entities()
+        # time.sleep(2)
+        # self.load.cleanDel()
+if __name__ == '__main__':
+    game = Game(spawn=Vec3(0,0,0))
+
+
+    game.run()
---
+++ scripts/mainn_menu.py
@@ -0,0 +1,41 @@
+from ursina import *
+
+
+class MainMenu:
+    def __init__(self, play):
+        self.bg = Entity(
+            model='quad',
+            texture='bg',
+            scale=Vec3(15, 8.5, 0),
+        )
+        self.play = Button(
+            text='play',
+            scale=(.25, .075),
+            color=color.azure,
+            on_click=play,
+            tooltip=Tooltip("Play")
+        )
+        self.quit = Button(
+            text='quit',
+            scale=(.25, .075),
+            color=color.azure,
+            on_click=Sequence(Wait(.01), Func(sys.exit)),
+            position= self.play.position + Vec3(0, -0.1, 0)
+        )
+        # self.settings = Button(
+        #     texture='btn_sett',
+        #     position=Vec3(),
+        #     enabled=False,
+        # )
+        # self.quit = Button(
+        #     texture='btn_quit',
+        #     position=Vec3(),
+        #     enabled=False,
+        # )
+
+    def cleanDel(self):
+        destroy(self.bg)
+        destroy(self.play)
+        # destroy(self.settings)
+        # destroy(self.quit)
+        del self
