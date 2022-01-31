a3977897b8e8d530b8d5785e5ba8f23eacb04c0f
elokrabek <mikus.taki2005@onet.pl>
2022-01-31 23:26:15 +0100

    beryl

--- scripts/gra.py
+++ scripts/gra.py
@@ -14,13 +14,13 @@ player = FirstPersonController(x=-4, z=-2, rotation=(0, 180, 0), scale=1.5)
 player.cursor = LitObject(parent=camera.ui, model="circle", color=color.black, scale=.012)
 
 
-# window.fullscreen = True
+window.fullscreen = True
 
 
 def light():
     moon = LitDirectionalLight(direction=Vec3(1.6, -1, 0), color=color.gray, intensity=0.15)
-    lampka2 = LitSpotLight(position=Vec3(-1.9, 2.71, -4.55), range=0.5, intensity=1, direction=Vec3(-0.7, -2, 0.7),
-                           angle=30)
+    #lampka2 = LitSpotLight(position=Vec3(-1.9, 2.71, -4.55), range=0.5, intensity=1, direction=Vec3(-0.7, -2, 0.7),
+                          # angle=30)
     lampka22 = LitPointLight(position=Vec3(-1.9, 2.71, -4.55), range=0.1, intensity=0.5)
     # lampka1 =(position=Vec3(0, 4.7, -1), range=3, intensity=10, direction=Vec3(0, -2, 0), angle=50)
     # test= LitObject(model="cube", position=Vec3(0, 4.8, -1), color=color.light_gray, scale=0.1)
